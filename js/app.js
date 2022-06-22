f29768f5919c9d46a2dfe6d86912fdfc29ab2bc2
jose landy giorio do vale <joselandy@gmail.com>
2022-06-21 23:02:51 -0300

    Playing Notes
    Keys playing notes correctly. Using globas vars in app.js to change dinamically css values
    Keydown acivates the css change to simulate key press

--- js/app.js
+++ js/app.js
@@ -1,16 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => { //this allow the html script call to be in the begining of the file, before the HTML content.
+    //Global variables declaration
     const keys = document.querySelectorAll('.key')
+    let keyClass
+
     keys.forEach((key => {
-        const keyPlay = key.dataset.note
+
+        const keyPlay = key.dataset.key
+        const note    = key.dataset.note
 
         document.addEventListener('keydown', (e) => {
             const keyPressed = e.code
             if(keyPressed == keyPlay){
-                console.log("Tocou: ", keyPlay)
-            }
-        })
-        
-    }))
-    
 
+                if(note.substr(1,1) =='-'){
+                    keyClass = "black-active"
+                }else{
+                    keyClass = "white-active"
+                }
+                
+                const noteSound = new Audio('../samples/'+note+'.mp3')
+                const classAdd = key.classList.add(keyClass)
+                const classRemove = setTimeout(removeClass,100)
+
+                noteSound.play()
+                         
+                function removeClass(){
+                    key.classList.remove(keyClass)
+                }
+            }
+        }) 
+    }))
 })
